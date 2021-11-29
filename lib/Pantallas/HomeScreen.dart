2e1b4ff555b4c92457b90e536061f600a2852d9d
albertcollado <albert.collado.camps@estudiantat.upc.edu>
2021-11-29 16:39:58 +0100

    Mejorado HomeScreen

--- lib/Pantallas/HomeScreen.dart
+++ lib/Pantallas/HomeScreen.dart
@@ -1,8 +1,8 @@
-// ignore: file_names
-// ignore_for_file: non_constant_identifier_names, prefer_const_constructors, file_names
+// ignore_for_file: non_constant_identifier_names, prefer_const_constructors, file_names, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:projecte/widgets/Usuari.dart';
+import 'package:flutter/rendering.dart';
+//import 'package:projecte/widgets/Usuari.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,7 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late Usuari user;
+  //late Usuari user;
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.grey[900],
       body: Column(
         mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
@@ -38,57 +39,121 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 Row(
                   children: [
-                    Expanded(
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
                       child: Text(
                         'GameApp',
-                        style: TextStyle(fontSize: 30),
+                        style: TextStyle(fontSize: 30, color: Colors.white),
                       ),
-                    )
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: Icon(
+                        Icons.search_rounded,
+                        color: Colors.grey,
+                      ),
+                    ),
                   ],
                 ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    height: 65,
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.black, spreadRadius: 5, blurRadius: 5)
-                      ],
-                      color: Colors.grey[850],
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20),
-                      ),
-                    ),
-                    child: Expanded(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(
-                            Icons.home_rounded,
-                            color: Colors.grey,
-                            size: 35,
-                          ),
-                          Icon(
-                            Icons.grid_view_outlined,
-                            color: Colors.grey,
-                            size: 35,
-                          ),
-                          Icon(
-                            Icons.person_rounded,
-                            color: Colors.grey,
-                            size: 35,
-                          ),
-                        ],
-                      ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+                  child: Text(
+                    'Últims jocs',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Aqui van los últimos juegos',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  width: 100,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black, spreadRadius: 5, blurRadius: 5)
+                    ],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
                     ),
                   ),
-                )
+                ),
+                SizedBox(height: 25),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+                  child: Text(
+                    'Per a tu',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Aqui van los juegos para ti',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  width: 100,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black, spreadRadius: 5, blurRadius: 5)
+                    ],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 65,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(color: Colors.black, spreadRadius: 5, blurRadius: 5)
+                ],
+                color: Colors.grey[850],
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.home_rounded,
+                      color: Colors.grey,
+                      size: 35,
+                    ),
+                    Icon(
+                      Icons.grid_view_outlined,
+                      color: Colors.grey,
+                      size: 35,
+                    ),
+                    Icon(
+                      Icons.person_rounded,
+                      color: Colors.grey,
+                      size: 35,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
