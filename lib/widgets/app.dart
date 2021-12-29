60237f01e27ab773b24f38e17bab6d02cdbf4466
albertcollado <albert.collado.camps@estudiantat.upc.edu>
2021-12-29 18:23:37 +0100

    comentario en app

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+
+
+//APP de PAUEK LOGIN
+
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
